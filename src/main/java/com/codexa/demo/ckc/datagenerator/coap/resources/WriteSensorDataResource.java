3cbafb5424a7d87f21dc0c7d16bf451cb4f709c8
peter <peter.szatmary@codexa.sk>
2017-08-14 11:39:52 +0200

    i am sending put messages to coap server. coap server saw them. sending me back message. Kafka connect blind git add .

--- src/main/java/com/codexa/demo/ckc/datagenerator/coap/resources/WriteSensorDataResource.java
+++ src/main/java/com/codexa/demo/ckc/datagenerator/coap/resources/WriteSensorDataResource.java
@@ -1,6 +1,7 @@
 package com.codexa.demo.ckc.datagenerator.coap.resources;
 
 import com.codexa.demo.ckc.tocassandra.SensorDataEntity;
+import com.codexa.demo.ckc.util.Data;
 import org.apache.commons.lang3.SerializationUtils;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.CoAP;
@@ -14,30 +15,34 @@ import static org.eclipse.californium.core.coap.CoAP.ResponseCode.CHANGED;
  */
 public class WriteSensorDataResource extends CoapResource {
 
-    public SensorDataEntity data;// = Data.getSensorMessage("dummy", "dummy", "dummy");
+    public SensorDataEntity data = Data.getSensorMessage("dummy", "dummy", "dummy");
+
+    private long counter = 0;
 
     public WriteSensorDataResource() {
         super("write-sensor-data");
-        //data = Data.getSensorMessage("type", "value", "device");
     }
-
-    //TODO : org.apache.commons.lang3.SerializationException: java.io.StreamCorruptedException: invalid stream header: 74796874
 
     @Override
     public void handleGET(CoapExchange exchange) {
         exchange.respond(CoAP.ResponseCode.CONTENT, SerializationUtils.serialize(data));
+        System.out.println("handleGET responds with " + data);
     }
 
     @Override
     public void handlePUT(CoapExchange exchange) {
 
         byte[] payload = exchange.getRequestPayload();
+
         try {
             //data = SerializationUtils.deserialize(payload);
             exchange.respond(CHANGED, payload);
+            exchange.getRequestText();
+            System.out.println("From server : yes i see your PUT " + (++counter) + "= " + payload);
         } catch (Exception e) {
             e.printStackTrace();
             exchange.respond(BAD_REQUEST, "Invalid sensor data");
+            System.out.println("From server : your PUIT is wrong !");
         }
     }
 
